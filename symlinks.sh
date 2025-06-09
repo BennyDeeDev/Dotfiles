57f020751df259c9fbb04555aa3bbcd7dad5c3c0
Benjamin Derksen <benjamin.derksen.98@gmail.com>
2025-06-09 19:44:49 +0200

    fix recursive symlink bug

--- symlinks.sh
+++ symlinks.sh
@@ -2,10 +2,10 @@
 
 DOTFILES="$HOME/Repos/dotfiles-arch"
 
-ln -sf "$DOTFILES/nvim" ~/.config/nvim
-ln -sf "$DOTFILES/i3" ~/.config/i3
-ln -sf "$DOTFILES/alacritty" ~/.config/alacritty
-ln -sf "$DOTFILES/rofi" ~/.config/rofi
-ln -sf "$DOTFILES/polybar" ~/.config/polybar
-ln -sf "$DOTFILES/.zshrc" ~/.zshrc
-ln -sf "$DOTFILES/.p10k.zsh" ~/.p10k.zsh
+ln -sfn "$DOTFILES/nvim" ~/.config/nvim
+ln -sfn "$DOTFILES/i3" ~/.config/i3
+ln -sfn "$DOTFILES/alacritty" ~/.config/alacritty
+ln -sfn "$DOTFILES/rofi" ~/.config/rofi
+ln -sfn "$DOTFILES/polybar" ~/.config/polybar
+ln -sfn "$DOTFILES/.zshrc" ~/.zshrc
+ln -sfn "$DOTFILES/.p10k.zsh" ~/.p10k.zsh
