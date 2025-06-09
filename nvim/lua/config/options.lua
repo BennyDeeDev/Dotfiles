bf82e5192ce1a64cf84100f0d8c4f487df373567
Benjamin Derksen <benjamin.derksen.98@gmail.com>
2025-06-09 23:17:40 +0200

    includ vim scrolloff and titlestring

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.scrolloff = 16
+
+vim.o.title = true
+vim.o.titlestring = "%t – " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t") .. " – nvim"
