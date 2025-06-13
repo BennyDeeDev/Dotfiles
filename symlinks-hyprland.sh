cb53c45db744702e357c5e01e4105c6d87e529b3
Benjamin Derksen <benjamin.derksen.98@gmail.com>
2025-06-13 21:14:37 +0200

    add hypr and wayland to symlink script

--- symlinks-hyprland.sh
+++ symlinks-hyprland.sh
@@ -2,6 +2,8 @@
 
 DOTFILES="$HOME/Repos/dotfiles-linux"
 
+ln -sfn "$DOTFILES/hypr" ~/.config/hypr
+ln -sfn "$DOTFILES/waybar" ~/.config/waybar
 ln -sfn "$DOTFILES/nvim" ~/.config/nvim
 ln -sfn "$DOTFILES/alacritty" ~/.config/alacritty
 ln -sfn "$DOTFILES/.zshrc" ~/.zshrc
