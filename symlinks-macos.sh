95d863d6f209a20aa5d620ed47504ec94115cc79
Benjamin Derksen <benjamin.derksen.98@gmail.com>
2025-06-15 16:08:50 +0200

    replace alacritty with ghostty

--- symlinks-macos.sh
+++ symlinks-macos.sh
@@ -4,7 +4,7 @@ DOTFILES="$HOME/Repos/dotfiles"
 
 ln -sfn "$DOTFILES/Brewfile" ~/Brewfile
 ln -sfn "$DOTFILES/nvim" ~/.config/nvim
-ln -sfn "$DOTFILES/alacritty" ~/.config/alacritty
+ln -sfn "$DOTFILES/ghostty" ~/.config/ghostty
 ln -sfn "$DOTFILES/.zshrc.macos" ~/.zshrc
 ln -sfn "$DOTFILES/.zprofile.macos" ~/.zprofile
 ln -sfn "$DOTFILES/.p10k.zsh" ~/.p10k.zsh
