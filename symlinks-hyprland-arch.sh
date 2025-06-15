8aaca9f7029d1c591d16047cc4f0a01d0be4ea0f
Benjamin Derksen <benjamin.derksen.98@gmail.com>
2025-06-15 21:56:27 +0200

    setup ghostty in arch

--- symlinks-hyprland-arch.sh
+++ symlinks-hyprland-arch.sh
@@ -5,6 +5,6 @@ DOTFILES="$HOME/Repos/dotfiles"
 ln -sfn "$DOTFILES/hypr" ~/.config/hypr
 ln -sfn "$DOTFILES/waybar" ~/.config/waybar
 ln -sfn "$DOTFILES/nvim" ~/.config/nvim
-ln -sfn "$DOTFILES/alacritty" ~/.config/alacritty
+ln -sfn "$DOTFILES/ghostty" ~/.config/ghostty
 ln -sfn "$DOTFILES/.zshrc.arch" ~/.zshrc
 ln -sfn "$DOTFILES/.p10k.zsh" ~/.p10k.zsh
